e740934ba2c45ed497f007919742221d16003668
Jaay V. Joshhi <jamjayjoshi108@gmail.com>
2026-03-06 18:26:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from streamlit_gsheets import GSheetsConnection
 
 # -----------------------------------------------------------------------------
 # 1. PAGE CONFIGURATION & CUSTOM CSS (State-of-the-art look)
